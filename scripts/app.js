aa684b0f2cefe6c311594465deec82013db9c320
Kikko Paradela <kikkoooo@users.noreply.github.com>
2017-01-25 14:39:57 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){     
-	// $("body").html(timer.month + '/' + timer.date + '/' + timer.year + ' ' + timer.hour + ':' + timer.minutes + ':' + timer.seconds + ' ' + timer.meridiem + ' ' + timer.day);
-	setInterval(function() {
-		$("body").html(timer.month + '/' + timer.date + '/' + timer.year + ' ' + timer.hour + ':' + timer.minutes + ':' + timer.seconds + ' ' + timer.meridiem + ' ' + timer.day);
-	}, 1000);
+	$("body").append("<h1>HELLO</h1>");
 });
